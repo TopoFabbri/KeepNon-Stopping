f84d141057d9984078c37bed4ae272105c68c1b9
Mateo <matufabbri@gmail.com>
2022-10-24 17:46:47 -0300

    Added bg

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
 function love.draw()
     love.graphics.setColor(.1, .5, .6, 1)
-    love.graphics.rectangle('fill', 0, 0, 300, 388)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 end
