da17869cd49e0a6cbdac45db659a41f3aabde3a8
Mateo <matufabbri@gmail.com>
2022-10-24 22:39:36 -0300

    Added falling to avoidables

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 local player = {}
 local avoidables = {}
+local avoidableCounter = 0
+local max = 100
 
 for i = 1, 100 do
     avoidables[i] = {}
@@ -7,8 +9,8 @@ end
 
 function love.load()
 
-    createAvoidable()
-    
+    createAvoidables()
+
     player.pos = {
         x = love.graphics.getWidth() / 2,
         y = love.graphics.getHeight() * (7 / 8)
@@ -26,7 +28,6 @@ end
 
 function love.update(dt)
 
-
     if love.keyboard.isDown('right') then
         player.pos.x = player.pos.x + player.speed * dt
     end
@@ -34,17 +35,18 @@ function love.update(dt)
         player.pos.x = player.pos.x - player.speed * dt
     end
     updateVertices()
+    updateAvoidables(dt)
 end
 
 function love.draw()
-    
-    love.graphics.setColor(.1, .5, .6, 1)
-    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-    love.graphics.setColor(0, 0, 0, 1)
-    love.graphics.polygon("fill", player.tri)
 
-    for i = 1, avoidables.max do
-        love.graphics.setColor(0, 0, 0, 1)
+    love.graphics.setColor(0, 0, 0, 1)
+    love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.polygon("line", player.tri)
+
+    for i = 1, max do
+        love.graphics.setColor(1, 1, 1, 1)
         love.graphics.circle("fill", avoidables[i].pos.x, avoidables[i].pos.y, avoidables[i].size)
     end
 end
@@ -55,22 +57,46 @@ function updateVertices()
 
 end
 
-function createAvoidable()
+function createAvoidables()
     local randomNumber
     local randomNumberSize
 
-    for i = 1, avoidables.max do
+    for i = 1, max do
+        createAvoidable(i)
+    end
+end
 
-        randomNumber = love.math.random(20)
-        randomNumberSize = love.math.random(10, 60)
-        
-        avoidables[i].speed = 30
-        avoidables[i].max = 100
-        avoidables[i].size = randomNumberSize
+function createAvoidable(i)
+    avoidables[i].isActive = false
+    avoidables[i].counter = .1 * i
 
-        avoidables[i].pos = {
-            x = love.graphics.getWidth() *  (randomNumber / 20),
-            y = love.graphics.getHeight() * 0 - 50
-        }
+    randomNumber = love.math.random(20)
+    randomNumberSize = love.math.random(10, 60)
+
+    avoidables[i].speed = 300
+    avoidables[i].size = randomNumberSize
+
+    avoidables[i].pos = {
+        x = love.graphics.getWidth() * (randomNumber / 20),
+        y = love.graphics.getHeight() * 0 - randomNumberSize / 2
+    }
+
+end
+
+function updateAvoidables(dt)
+    for i = 1, max do
+        if avoidables[i].counter > 0 then
+            avoidables[i].counter = avoidables[i].counter - dt
+        else
+            avoidables[i].isActive = true
+            avoidableCounter = avoidableCounter + 1
+        end
+        fall(dt, i)
+    end
+end
+
+function fall(dt, i)
+    if avoidables[i].isActive then
+        avoidables[i].pos.y = avoidables[i].pos.y + avoidables[i].speed * dt
     end
 end
